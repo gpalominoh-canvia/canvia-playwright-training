4b845a5f0fd97424323767e32466fa0a4af42736
andreaMonica <andreamcondorihuayunga@gmail.com>
2026-06-10 10:11:45 -0500

    refactor: limpieza en cart.spect.ts

--- tests/nivel-2-pom/cart.spec.ts
+++ tests/nivel-2-pom/cart.spec.ts
@@ -5,12 +5,10 @@ import { products } from '@pom/data/products';
 test.describe('Carrito de compras', () => {
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.open();
-    // Agrega esta línea para realizar la autenticación antes de cada test
     await loginPage.login(users.standard.username, users.standard.password);
   });
 
   test('Agregar productos al carrito y verificar cantidad', async ({ inventoryPage, cartPage }) => {
-    // Ahora sí estás dentro de la página de inventario
     await inventoryPage.addProductToCart(products.backpack);
     await inventoryPage.openCart();
 
