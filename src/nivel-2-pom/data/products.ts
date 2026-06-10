979f45caf762d139121dd52413ba14f08a1b5777
cocuss <lunacostudios@gmail.com>
2026-06-10 12:59:05 -0500

    feature/marco-nivel02-pom

---
+++ src/nivel-2-pom/data/products.ts
@@ -0,0 +1,9 @@
+export const products={
+    backpack:'Sauce Labs Backpack',
+    bikelight:'Sauce Labs Bike Light',
+    bolttshirt: 'Sauce Labs Bolt T-Shirt',
+    fleecejacket:'Sauce Labs Fleece Jacket',
+    onesie:'Sauce Labs Onesie',
+    testAllThings:'Test.allTheThings() T-Shirt (Red)',
+
+} as const;
